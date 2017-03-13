0cd7032c35eaad290c8ce3e065b9a37374ef356d
huangruichang <532079207@qq.com>
2017-03-13 11:18:05 +0800

    + add production build scripts

---
+++ webpack.dev.js
@@ -0,0 +1,76 @@
+'use strict'
+
+const resolve = require('path').resolve
+const process = require('process')
+const webpack = require('webpack')
+const NotifierPlugin = require('webpack-notifier')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const appDir = resolve(__dirname, './app/src')
+
+module.exports = {
+
+  // https://github.com/webpack/webpack/issues/1599#issuecomment-186841345
+  // __dirname returns '/' when js file is built with webpack
+  target: 'node',
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+
+  entry: {
+    'index.js': './app/src/index.js',
+    'entry.js': './app/src/entry.js',
+  },
+
+  devtool: "source-map",
+  //devtool: 'cheap-module-source-map',
+  //devtool: 'eval',
+
+  output: {
+    pathinfo: true,
+    path: './app/dist',
+    filename: '[name]',
+    sourceMapFilename: '[name].map',
+  },
+
+  module: {
+    preLoaders: [
+      { test: /\.js$/, include: appDir, loader: 'eslint' },
+    ],
+    loaders: [
+      { test: /\.json$/, include: appDir, loader: 'json' },
+      { test: /\.js$/, include: appDir, loader: 'babel' },
+      { test: /\.scss$/, include: appDir, loaders: ["style", "css?modules&sourceMap", "sass?sourceMap"] },
+      { test: /.node$/, include: appDir, loader: 'node' },
+      { test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100000&name=[path][name].[ext]'}
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js'],
+    packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main'],
+  },
+
+  externals: [
+    (ctx, req, cb) => {
+      if (/^[electron|react|nodegit]/.test(req)) {
+        return cb(null, `commonjs ${req}`)
+      }
+      cb()
+    },
+  ],
+
+  plugins: [
+    new NotifierPlugin({ alwaysNotify: true }),
+    new webpack.NoErrorsPlugin(),
+    new CopyWebpackPlugin([{
+      from: resolve(appDir, 'index.html'),
+      to: 'index.html'
+    }]),
+    new webpack.ProvidePlugin({
+      Promise: 'bluebird',
+    }),
+    new webpack.DefinePlugin({
+      __DEVTOOLS__: true,  // <-------- DISABLE redux-devtools HERE
+    }),
+  ],
+}
